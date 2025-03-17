63133be12f752f5b43df3352a046fead2457c604
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-17 18:05:30 +0800

    Create conveyorControl.h

---
+++ conveyorControl.h
@@ -0,0 +1,11 @@
+#ifndef __CONVEYORCONTROL__H
+#define __CONVEYORCONTROL__H
+
+void initConveyor();
+void conveyorForward_Manure();
+void conveyorReverse_Manure();
+void conveyorForward_Collect();
+void conveyorStop();
+void runConveyor();
+
+#endif
