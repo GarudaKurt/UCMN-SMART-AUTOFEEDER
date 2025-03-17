e96ef7e4f9a3a1f95c9ff171046314b207742f70
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-17 18:00:44 +0800

    Update CPE-SMART-FEEDER.ino

--- CPE-SMART-FEEDER.ino
+++ CPE-SMART-FEEDER.ino
@@ -2,19 +2,19 @@
 #include "display.h"
 #include "weightObject.h"
 #include "timer.h"
-
+#include "conveyorControl.h"
 
 void setup() {
   Serial.begin(115200);
   initDisplay();
   initWeight();
   initTimer();
+  initConveyor();
   delay(2000);
 }
 
 void loop() {
   float weight = getWeightValue();
-  const char * time = getCurrentTime();
+  const char *time = getCurrentTime();
   showDisplay(time, weight);
-  delay(1000);
 }
