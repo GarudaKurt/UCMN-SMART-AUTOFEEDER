bcaa0a3611f3e060b2e8207ee44e43ceae187a7f
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-31 15:37:02 +0800

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -1,9 +1,12 @@
 #include "display.h"
+#include "myservo.h"
+
 #include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 static bool sorted = false;
+unsigned long prevTime = 0;
 
 static int cntS = 0;
 static int cntM = 0;
@@ -20,27 +23,57 @@ void initDisplay() {
 }
 
 void showDisplay(const char* time, float size) {
-  String type = "";
-  if (size >= 8.40 && size < 9) {
-    Serial.println("SMALL");
-    type = "SMALL";
-    cntS++;
-  } else if (size >= 9.35 && size < 10) {
-    Serial.println("MEDIUM");
-    type = "MEDIUM";
-    cntM++;
-  } else if(size >= 10  && size < 11.00) {
-    Serial.println("LARGE");
-    type = "LARGE";
-    cntL++;
+  static int step = 0; // Track servo sequence steps
+  static unsigned long actionTime = 0; // Store last action timestamp
+  static unsigned long prevTime = 0;
+  String type = ""; 
+  if(millis() - prevTime > 2000) {
+    prevTime = millis();
+    if (size >= 8.40 && size < 9) {
+      Serial.println("SMALL");
+      type = "SMALL";
+      sorted = true;
+      cntS++;
+    } else if (size >= 9.35 && size < 10) {
+      Serial.println("MEDIUM");
+      type = "MEDIUM";
+      sorted = true;
+      cntM++;
+    } else if (size >= 10  && size < 11.00) {
+      Serial.println("LARGE");
+      type = "LARGE";
+      sorted = true;
+      cntL++;
+    }
+
+  }
+  
+  if (sorted) {    
+    servoStop();
+    delay(1000);
+    resetStop();
+    if(type == "SMALL") {
+      servoSorting(90);
+    } else if(type == "MEDIUM") {
+      servoSorting(45);
+    } else if(type ==" LARGE") {
+      servoSorting(130);
+    }
+    servoDischarge();
+    delay(1000);
+    sorted = false;
+    resetSorting();
+    resetDischarge();
   }
 
+  // Display update
   lcd.setCursor(3, 0);
-  lcd.print("Smart Poultry    ");
+  lcd.print("Smart Poultry    "); 
+
   lcd.setCursor(3, 1);
   lcd.print("Time: ");
   lcd.print(time);
-  lcd.print("    ");
+  lcd.print("    ");  
 
   lcd.setCursor(1, 2);
   lcd.print("EGG TYPE: ");
@@ -54,6 +87,5 @@ void showDisplay(const char* time, float size) {
   lcd.print(cntM);
   lcd.print(" LG:");
   lcd.print(cntL);
-  lcd.print("   ");
+  lcd.print("   ");  
 }
-
