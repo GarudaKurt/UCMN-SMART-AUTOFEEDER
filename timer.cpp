b6241a5c309e7a6425a3ccc59ac3fbf785229944
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-31 15:37:36 +0800

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,6 +1,6 @@
 #include "timer.h"
 #include "conveyorControl.h"
-
+#include "myservo.h"
 #include <RTClib.h>
 #include <Arduino.h>
 
@@ -11,9 +11,19 @@ RTC_DS1307 rtc;
 #define relay_water 12
 
 bool conveyorActive = false;
+bool uvLightActive = false;
+bool waterActive = false;
+
+unsigned long uvStartTime = 0;
+unsigned long waterStartTime = 0;
+
+const unsigned long uvRunDuration = 120000; // 2 minutes (120,000 ms)
+const unsigned long uvCycleInterval = 300000; // 5 minutes (300,000 ms)
+const unsigned long waterRunDuration = 10000; // 10 seconds (10,000 ms)
+const unsigned long waterCycleInterval = 180000; // 3 minutes (180,000 ms)
+const unsigned long conveyorRunDuration = 40000; // 40 seconds
 
 void initTimer() {
-    // Check if RTC is connected
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
@@ -27,37 +37,21 @@ void initTimer() {
   digitalWrite(relay_water, HIGH);
 }
 
-static void lampON() {
-  Serial.println("Light bulb ON!");
-  digitalWrite(relay_lightsLamp, LOW);
-}
-static void lampOFF() {
-  Serial.println("Light bulb OFF!");
-  digitalWrite(relay_lightsLamp, HIGH);
-}
-static void uvlightON() {
-  digitalWrite(relay_lightsUV, LOW);
-}
-static void uvlightOFF() {
-  digitalWrite(relay_lightsUV, HIGH);
-}
-static void waterON() {
-  digitalWrite(relay_water, LOW);
-}
-static void waterOFF() {
-  digitalWrite(relay_water, HIGH);
-}
+static void lampON() { digitalWrite(relay_lightsLamp, LOW); }
+static void lampOFF() { digitalWrite(relay_lightsLamp, HIGH); }
+static void uvlightON() { digitalWrite(relay_lightsUV, LOW); }
+static void uvlightOFF() { digitalWrite(relay_lightsUV, HIGH); }
+static void waterON() { digitalWrite(relay_water, LOW); }
+static void waterOFF() { digitalWrite(relay_water, HIGH); }
 
 const char* getCurrentTime() {
   static char timeStr[6];
   static int lastExecutedMinute = -1;
   static bool isConveyorRunning = false;
-  static unsigned long conveyorStartTime = 0;
-  const unsigned long runDuration = 40000; 
 
-  static bool uvLightActive = false;
+  static unsigned long conveyorStartTime = 0;  // Fix: Make it static
   static unsigned long uvStartTime = 0;
-  const unsigned long uvRunDuration = 120000;
+  static unsigned long waterStartTime = 0;
 
   DateTime now = rtc.now();
   int currentMinutes = now.minute();
@@ -69,30 +63,49 @@ const char* getCurrentTime() {
   Serial.print(" | Seconds: ");
   Serial.println(currentSeconds);
 
-  // Start UV light every 5 minutes
-  if (!uvLightActive && currentMinutes % 5 == 0 && currentMinutes != lastExecutedMinute) {
+  // UV Light Control
+  if (!uvLightActive && (currentMillis - uvStartTime >= uvCycleInterval)) {
     Serial.println("Turning UV Light ON.");
     uvlightON();
-    waterON();
+    lampON();
     uvLightActive = true;
     uvStartTime = currentMillis;
-    lastExecutedMinute = currentMinutes;
+  }
+  if (uvLightActive && (currentMillis - uvStartTime >= uvRunDuration)) {
+    Serial.println("Turning UV Light OFF.");
+    uvlightOFF();
+    lampOFF();
+    uvLightActive = false;
   }
 
+  // Water Control
+  if (!waterActive && (currentMillis - waterStartTime >= waterCycleInterval)) {
+    Serial.println("Turning Water ON.");
+    feederON();
+    waterON();
+    waterActive = true;
+    waterStartTime = currentMillis;
+  }
+  if (waterActive && (currentMillis - waterStartTime >= waterRunDuration)) {
+    Serial.println("Turning Water OFF.");
+    waterOFF();
+    feederOFF();
+    waterActive = false;
+  }
+
+  // Conveyor Control
   if (!isConveyorRunning && currentMinutes % 2 == 0 && currentMinutes != lastExecutedMinute) {
-    Serial.println("Starting conveyor.");
+    Serial.println("Run conveyor!");
     runConveyor();
-    lampON();
-    isConveyorRunning = true;  
-    conveyorStartTime = currentMillis; 
-    lastExecutedMinute = currentMinutes; 
-  // We stop the conveyor after 10 seconds
-  if (isConveyorRunning && (currentMillis - conveyorStartTime >= runDuration)) {
+    isConveyorRunning = true;
+    conveyorStartTime = currentMillis;
+    lastExecutedMinute = currentMinutes;
+  }
+  if (isConveyorRunning && (currentMillis - conveyorStartTime >= conveyorRunDuration)) {
     Serial.println("Stopping conveyor.");
-    conveyorStop(); 
-    lampOFF();
+    conveyorStop();
     isConveyorRunning = false;
-  } 
+  }
 
   sprintf(timeStr, "%02d:%02d", currentMinutes, currentSeconds);
   return timeStr;
