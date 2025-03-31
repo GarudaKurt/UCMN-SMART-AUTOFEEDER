0abf721f0a59f4c22adf46b2a701c57565ca57df
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-31 15:42:09 +0800

    Create myservo.h

---
+++ myservo.h
@@ -0,0 +1,15 @@
+#ifndef __MYSERVO__H
+#define __MYSERVO__H
+
+void initServo();
+void servoDischarge();
+void resetDischarge();
+void servoStop();
+void resetStop();
+void feederON();
+void feederOFF();
+void servoSorting(int angle);
+void resetSorting();
+
+
+#endif
