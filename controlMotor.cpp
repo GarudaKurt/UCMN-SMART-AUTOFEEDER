d28126ac25856286c090f86d28ca4b3f1009ae98
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-04-09 17:37:06 +0800

    Update controlMotor.cpp

--- controlMotor.cpp
+++ controlMotor.cpp
@@ -3,11 +3,31 @@
 #include <Wire.h>
 #include <AccelStepper.h>
 
-#define DIR_PIN 2
-#define STEP_PIN 3
-#define ENABLE_PIN 4
+// Define stepper pins
+#define STEP_PIN 3      // Step pin
+#define DIR_PIN 2       // Direction pin
 
+// Microstepping control pins
+#define M0_PIN 7
+#define M1_PIN 6
+#define M2_PIN 5
+
+// Steps per revolution for the motor
+const float stepsPerRevolution = 200;
+// Microstepping multiplier (1, 2, 4, 8, 16, or 32)
+int microstepSetting = 1;
+
+// Set the desired RPM and the max RPM
+float desiredRPM = 60; // Set the desired speed in rpm (revolutions per minute)
+float MaxRPM = 300; // Set max speed in rpm (revolutions per minute)
+
+// Calculate and set the desired and max speed in steps per second
+float speedStepsPerSec = (microstepSetting * stepsPerRevolution*desiredRPM) / 60.0;
+float Max_Speed_StepsPerSec = microstepSetting * stepsPerRevolution * MaxRPM / 60; // Specify max speed in steps/sec (converted from RPM)
+
+// AccelStepper instance in driver mode
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
 
 unsigned long prevStepperTime = 0;
 unsigned long stepperStartTime = 0;
@@ -15,26 +35,32 @@ bool stepperRunning = false;
 
 void initStepperConveyor() {
   Wire.begin();
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(500);
-  pinMode(ENABLE_PIN, OUTPUT);
-  digitalWrite(ENABLE_PIN, LOW);  // Enable driver
+  pinMode(M0_PIN, OUTPUT);
+  pinMode(M1_PIN, OUTPUT);
+  pinMode(M2_PIN, OUTPUT);
+
+  // Set microstepping mode (adjust as needed: HIGH or LOW)
+  digitalWrite(M0_PIN, LOW);  // Set to LOW or HIGH for desired microstep setting
+  digitalWrite(M1_PIN, LOW);  // Set to LOW or HIGH for desired microstep setting
+  digitalWrite(M2_PIN, LOW);  // Set to LOW or HIGH for desired microstep setting
+
 }
 
 void conveyorForward_Collect_Stepper() {
   if (millis() - prevStepperTime >= 1000) {
     prevStepperTime = millis();
     Serial.println("Stepper Forward");
-    stepper.move(800);  // Adjust steps per second here
+    stepper.setMaxSpeed(Max_Speed_StepsPerSec);
+    stepper.setSpeed(speedStepsPerSec);
   }
-  stepper.run();
+  stepper.runSpeed();
 }
 
 void conveyorStop_Stepper() {
   if (millis() - stepperStartTime >= 40000) {
     Serial.println("Stepper Stopped");
     stepper.stop();
-    digitalWrite(ENABLE_PIN, HIGH); // Disable motor driver
+    //digitalWrite(ENABLE_PIN, HIGH); // Disable motor driver
     stepperRunning = false;
   }
 }
@@ -44,9 +70,11 @@ void runStepperConveyor() {
     stepperRunning = true;
     stepperStartTime = millis();
     Serial.println("Starting Stepper Conveyor...");
-    digitalWrite(ENABLE_PIN, LOW); // Enable driver
+   // digitalWrite(ENABLE_PIN, LOW); // Enable driver
   }
 
   conveyorForward_Collect_Stepper();
   conveyorStop_Stepper();
 }
+
+
